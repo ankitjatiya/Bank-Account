268dfded812da8ef640e2196ff5362d90929b817
Ankit Jatiya <ankitjatiya@Ankits-MacBook-Pro.local>
2023-09-19 08:12:33 +0530

    Added env variables using file

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bank_project/api"
+	db "bank_project/db/sqlc"
+	"bank_project/db/util"
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Error loading config file: ", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("Cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("Cannot start server:", err)
+	}
+}
